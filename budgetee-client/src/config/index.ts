4e56bba513dd875902ccc3d8ff6f542cad8072a4
dani-lp <danilau11@gmail.com>
2022-03-29 13:42:19 +0200

    Axios default config

--- budgetee-client/src/config/index.ts
+++ budgetee-client/src/config/index.ts
@@ -1,1 +1,1 @@
-export {}
+export const API_URL = process.env.REACT_APP_API_URL as string;
--- budgetee-client/src/features/dashboard/components/DashboardWidget.tsx
+++ budgetee-client/src/features/dashboard/components/DashboardWidget.tsx
@@ -7,7 +7,7 @@ type DashboardWidgetProps = {
 
 export const DashboardWidget = ({ title, children }: DashboardWidgetProps) => {
   return (
-    <div className='px-2 bg-white shadow-sm shadow-slate-400 col-span-1 rounded w-full overflow-hidden h-32 divide-y divide-gray-200'>
+    <div className='px-2 bg-white shadow-sm shadow-slate-400 col-span-1 rounded w-full overflow-hidden h-56 divide-y divide-gray-200'>
       <h2 className='py-1 font-medium text-lg'>{title}</h2>
       <div className='p-2'>
         {children}
